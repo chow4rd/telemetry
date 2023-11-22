3e650da77d018898a64a6683eb2930340b263139
rtanw10 <artwithrimjim@gmail.com>
2023-11-22 15:33:35 +0000

    Added New Button for Adding New Graphs

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import Upload from './pages/upload';
 import Visualise from './pages/visualise';
 import ViewData from './pages/viewData';
 import ErrorPage from './pages/errorPage';
+import AddNewGraph from "./pages/addNewGraph";
 import HomeImg from './icons/home.png';
 import UploadImg from './icons/upload.png';
 import ViewImg from './icons/view.png';
+import AddImg from './icons/add.png';
 
 function App() {
   
@@ -17,11 +19,13 @@ function App() {
         <Link to='/' className='sideImgContainer'><img className='sideImg' src={HomeImg} alt="visualise" /></Link>
         <Link to='/Upload' className='sideImgContainer'><img className='sideImg' src={UploadImg} alt="upload" /></Link>
         <Link to='/ViewData' className='sideImgContainer'><img className='sideImg' src={ViewImg} alt="" /></Link>
+        <Link to='/AddNewGraph' className='sideImgContainer'><img className='sideImg' src={AddImg} alt="" /></Link>
       </nav>
       <Routes>
         <Route path='/' element={<Visualise />}/>
         <Route path='/Upload' element={<Upload />}/>
         <Route path='/ViewData' element={<ViewData />}/>
+        <Route path='/AddNewGraph' element={<AddNewGraph />}/>
         <Route path='*' element={<ErrorPage />}/>
       </Routes>
     </Router>
---
+++ src/pages/addNewGraph.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AddNewGraph(){
+    return <div>this is addNewGraph</div>;
+}
+
+export default AddNewGraph;
