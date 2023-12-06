c6e62a6115ad141c3fcd2b84b9e838c83ab5c53e
chow4rd <choward3203@gmail.com>
2023-12-06 14:30:09 +0000

    Updates

--- src/pages/errorPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function ErrorPage() {
-    return <div className='errorMessage'>Error! This page does not exist <br></br><br></br>
-    <iframe title="cat" src="https://giphy.com/embed/jivGITd768psP80B2i" width="480" height="454" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-    </div>;
-}
-
-export default ErrorPage;
