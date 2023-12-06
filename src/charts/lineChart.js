e70864a46f857073e5a4f922de81fdeb551fdbf8
nouhaTamma <nouha.tamma@gmail.com>
2023-12-06 15:42:23 +0000

    add line chart js file

---
+++ src/charts/lineChart.js
@@ -0,0 +1,72 @@
+Highcharts.chart('container', {
+// From High charts but changes need to be made
+    title: {
+        text: 'U.S Solar Employment Growth',
+        align: 'left'
+    },
+
+    subtitle: {
+        text: 'By Job Category. Source: <a href="https://irecusa.org/programs/solar-jobs-census/" target="_blank">IREC</a>.',
+        align: 'left'
+    },
+
+    yAxis: {
+        title: {
+            text: 'Number of Employees'
+        }
+    },
+
+    xAxis: {
+        accessibility: {
+            rangeDescription: 'Range: 2010 to 2020'
+        }
+    },
+
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+    },
+
+    plotOptions: {
+        series: {
+            label: {
+                connectorAllowed: false
+            },
+            pointStart: 2010
+        }
+    },
+
+    series: [{
+        name: 'Installation & Developers',
+        data: data
+    }, {
+        name: 'Manufacturing',
+        data: data
+    }, {
+        name: 'Sales & Distribution',
+        data: data
+    }, {
+        name: 'Operations & Maintenance',
+        data: data
+    }, {
+        name: 'Other',
+        data: data
+    }],
+
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'bottom'
+                }
+            }
+        }]
+    }
+
+});
